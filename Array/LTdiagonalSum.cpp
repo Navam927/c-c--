5e3e48444eddfb642a0c9bc805e534f71268a4f9
Navam927 <navamsharma142@gmail.com>
2023-10-31 17:59:52 +0530

    Program to print sum / diagonal

---
+++ Array/LTdiagonalSum.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+using namespace std;
+#define size 3
+
+int main()
+{
+    int arr[size][size];
+    cout << "Enter elements to array: "<<endl;
+    for(int i = 0; i < size; i++){
+        for(int j = 0; j < size; j++){
+            cin>>arr[i][j];
+        }
+    }
+    cout<< "Array is "<<endl;
+    for(int i = 0; i < size ; i++){
+        for(int j = 0; j < size; j++){
+            cout<<arr[i][j]<<" ";
+        }
+        cout<<endl;
+    }
+    int sum = 0;
+    for(int i = 0; i < size; i++){
+        for(int j = 0; j < size; j++){
+            if(i+j==size-1){
+                sum += arr[i][j];
+            }
+        }
+    }
+    cout<<"sum of diagonal ele is "<<sum<<endl;
+    return 0;
+}
