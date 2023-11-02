36fa07e3d8ea9973c6f354b0328617e39dd23dd0
Navam927 <navamsharma142@gmail.com>
2023-11-02 09:39:20 +0530

    debugged the code

--- Array/wavePatter.cpp
+++ Array/wavePatter.cpp
@@ -1,32 +1,46 @@
 #include<iostream>
 using namespace std;
-#define size 3
-
+#define size 40
 int main()
 {
+    int n;
+    cout<<"Enter the size of nxn array ";
+    cin >> n;
+    cout<<endl;
+    cout<<"Enter elements to "<<n<<" x "<<n <<" array"<<endl;
     int arr[size][size];
-    cout << "Enter elements to array: "<<endl;
-    for(int i = 0; i < size; i++){
-        for(int j = 0; j < size; j++){
-            cin>>arr[i][j];
+    for(int i = 0; i < n; i++)
+    {
+        for(int j = 0; j < n; j++){
+            cin >> arr[i][j];
         }
     }
-    cout<< "Array is "<<endl;
-    for(int i = 0; i < size ; i++){
-        for(int j = 0; j < size; j++){
-            cout<<arr[i][j]<<" ";
-        }
+    cout<<"Your array is "<<endl;
+    for(int i = 0; i < n; i++)
+    {
+        for(int j = 0; j < n; j++)
+        {
+            cout << arr[i][j]<<" ";
+        } 
         cout<<endl;
     }
-    cout<<endl;
-    for(int j = 0; j < size; j++){
-        if(j%2==0){
-            for(int i = 0 ; i < size ; i++){
+
+    for(int j = 0; j < n; j++)
+    {
+        if(j%2==0)
+        {
+            for(int i = n-1; i >=0; i--)
+            {
                 cout<<arr[i][j]<<" ";
             }
-            cout<<endl;
+        }
+        else
+        {
+           for(int i = 0; i < n; i++)
+           {
+            cout<<arr[i][j]<<" ";
+           }
         }
     }
-    
     return 0;
 }
